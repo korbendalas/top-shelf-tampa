0468ddddd346f759a987f638648d0b4f5d8a529f
Dalibor <daliborpetric99@gmail.com>
2019-03-14 19:55:14 +0100

    Updates

---
+++ precache-manifest.10c9b8d0a5f8ad96c3156a0689af929a.js
@@ -0,0 +1,150 @@
+self.__precacheManifest = [
+  {
+    "revision": "50b507a358c1333bcabb47afc1dded3e",
+    "url": "/top-shelf-tampa/static/media/slider3.50b507a3.jpg"
+  },
+  {
+    "revision": "b6120c92863f503c747b",
+    "url": "/top-shelf-tampa/static/css/main.04af8356.chunk.css"
+  },
+  {
+    "revision": "e37ae64211326d463775",
+    "url": "/top-shelf-tampa/static/js/runtime~main.e37ae642.js"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/top-shelf-tampa/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "01ac11d41d6d7f742d25",
+    "url": "/top-shelf-tampa/static/js/2.01ac11d4.chunk.js"
+  },
+  {
+    "revision": "4d9e936f89bed5ae77d4ee4616a4fcdb",
+    "url": "/top-shelf-tampa/static/media/about-menu-img.4d9e936f.jpg"
+  },
+  {
+    "revision": "7bb61313971d990acf7f53e3563525cd",
+    "url": "/top-shelf-tampa/static/media/about-menu-img2.7bb61313.jpg"
+  },
+  {
+    "revision": "ef20a155097910acf3edc66a9d5b235f",
+    "url": "/top-shelf-tampa/static/media/food-img.ef20a155.jpg"
+  },
+  {
+    "revision": "758a9577514e86c66932d803bdf7c1f0",
+    "url": "/top-shelf-tampa/static/media/drinks-img.758a9577.jpg"
+  },
+  {
+    "revision": "b48c353723cd801703f0092db6e54cd1",
+    "url": "/top-shelf-tampa/static/media/Menu_Starters.b48c3537.jpg"
+  },
+  {
+    "revision": "129f4c48794f959456a80929836ea57a",
+    "url": "/top-shelf-tampa/static/media/Menu_Wings.129f4c48.jpg"
+  },
+  {
+    "revision": "dad34ce79c6e480b60608cd5c30356cb",
+    "url": "/top-shelf-tampa/static/media/Menu_Kids.dad34ce7.jpg"
+  },
+  {
+    "revision": "f5bcf512624057755862eca304fd1910",
+    "url": "/top-shelf-tampa/static/media/Menu_OffTheBench.f5bcf512.jpg"
+  },
+  {
+    "revision": "b707fafbb49f9c395811fd5a27a79c24",
+    "url": "/top-shelf-tampa/static/media/Menu_Sides.b707fafb.jpg"
+  },
+  {
+    "revision": "de0076f658629dbbc9073bb82bfe999e",
+    "url": "/top-shelf-tampa/static/media/logo_header.de0076f6.svg"
+  },
+  {
+    "revision": "588ee765b32f5716df486edf1f234aca",
+    "url": "/top-shelf-tampa/static/media/logo_footer.588ee765.svg"
+  },
+  {
+    "revision": "6512c9f0483e05f8c0c7836a2a11b6b2",
+    "url": "/top-shelf-tampa/static/media/slider1.6512c9f0.jpg"
+  },
+  {
+    "revision": "435e3b4a44cadc647c550c5b7a36fb3a",
+    "url": "/top-shelf-tampa/static/media/slider2.435e3b4a.jpg"
+  },
+  {
+    "revision": "b6120c92863f503c747b",
+    "url": "/top-shelf-tampa/static/js/main.b6120c92.chunk.js"
+  },
+  {
+    "revision": "664992b6e8f3c9451b9ec3570c748a05",
+    "url": "/top-shelf-tampa/static/media/slider4.664992b6.jpg"
+  },
+  {
+    "revision": "1c444aff21133dbc47bf6daef8a7628e",
+    "url": "/top-shelf-tampa/static/media/slider5.1c444aff.jpg"
+  },
+  {
+    "revision": "7fd6edb2b830166d7aa45fd389c4aba9",
+    "url": "/top-shelf-tampa/static/media/slider6.7fd6edb2.jpg"
+  },
+  {
+    "revision": "40114ac3309fc4d9c4e45ea73e2c485c",
+    "url": "/top-shelf-tampa/static/media/hockey-stick.40114ac3.png"
+  },
+  {
+    "revision": "28ab3087a6395918dcbd23f1a1880b03",
+    "url": "/top-shelf-tampa/static/media/snow-bg-top.28ab3087.png"
+  },
+  {
+    "revision": "52cfaedbb1e8f39b493fd444ea42fbc5",
+    "url": "/top-shelf-tampa/static/media/happy-hours-bg.52cfaedb.jpg"
+  },
+  {
+    "revision": "a1205631972da690e89f8aad4e245ada",
+    "url": "/top-shelf-tampa/static/media/menu_bcg.a1205631.jpg"
+  },
+  {
+    "revision": "c232fac3224ab963d4fdd3199897cd9c",
+    "url": "/top-shelf-tampa/static/media/drinks.c232fac3.jpg"
+  },
+  {
+    "revision": "83f1800183b5abd4a23896f8b18668cb",
+    "url": "/top-shelf-tampa/static/media/snow-bg.83f18001.jpg"
+  },
+  {
+    "revision": "9eec1d6ebb1a2e7dcbe62a586bacf135",
+    "url": "/top-shelf-tampa/static/media/catering_bcg.9eec1d6e.jpg"
+  },
+  {
+    "revision": "0e13ebf3f965504204cb0fdbf09478fc",
+    "url": "/top-shelf-tampa/static/media/team_bcg.0e13ebf3.jpg"
+  },
+  {
+    "revision": "64e930f157db0a4934488df9f914ae9d",
+    "url": "/top-shelf-tampa/static/media/about-top-bg.64e930f1.jpg"
+  },
+  {
+    "revision": "3ecaed158f840904f40e07ebe1eae70c",
+    "url": "/top-shelf-tampa/static/media/specials-bg.3ecaed15.jpg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/top-shelf-tampa/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/top-shelf-tampa/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/top-shelf-tampa/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "01ac11d41d6d7f742d25",
+    "url": "/top-shelf-tampa/static/css/2.38e1d74f.chunk.css"
+  },
+  {
+    "revision": "c6feefcfdec7ccfa8455bfba2157abdd",
+    "url": "/top-shelf-tampa/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/top-shelf-tampa/precache-manifest.10c9b8d0a5f8ad96c3156a0689af929a.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/top-shelf-tampa/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
